dd451e524b6a41d0accff01e13e1e5daef1ac5d6
Artin <artin.n31@gmail.com>
2023-08-30 13:07:18 -0700

    first commit

---
+++ linked-list.js
@@ -0,0 +1,98 @@
+/** Node: node for a singly linked list. */
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+/** LinkedList: chained together nodes. */
+
+class LinkedList {
+  constructor(vals = []) {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+
+    for (let val of vals) this.push(val);
+  }
+
+  /** push(val): add new value to end of list. */
+
+  push(val) {
+    let newNode = new Node(val);
+    if (this.head == null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length += 1;
+  }
+
+  /** unshift(val): add new value to start of list. */
+
+  unshift(val) {
+    let newNode = new Node(val);
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+
+    if (this.length === 0) this.tail = this.head;
+    this.length += 1;
+  }
+
+  /** pop(): return & remove last item. */
+
+  pop() {
+    if (this.length === 0) return null;
+    if (this.length === 1) {
+      this.head = this.tail;
+      this.length--;
+      return this.head.val;
+    }
+
+    let current = this.head;
+    let prev = null;
+    while (current.next) {
+      prev = current;
+      current = current.next;
+    }
+
+    prev.next = null;
+    this.tail = prev;
+    this.length--;
+    return current.val;
+  }
+
+  /** shift(): return & remove first item. */
+
+  shift() {}
+
+  /** getAt(idx): get val at idx. */
+
+  getAt(idx) {}
+
+  /** setAt(idx, val): set val at idx to val */
+
+  setAt(idx, val) {}
+
+  /** insertAt(idx, val): add node w/val before idx. */
+
+  insertAt(idx, val) {}
+
+  /** removeAt(idx): return & remove item at idx, */
+
+  removeAt(idx) {}
+
+  /** average(): return an average of all values in the list */
+
+  average() {}
+}
+
+module.exports = LinkedList;
